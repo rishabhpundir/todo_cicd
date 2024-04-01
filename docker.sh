e1e1a3804bfc4f47f9849685cb4761f9645b25d9
rishabhpundir <th3rishabh@gmail.com>
2024-04-01 12:41:16 +0530

    updated docker-compose once again

--- docker.sh
+++ docker.sh
@@ -2,5 +2,6 @@
 
 echo "********Docker Compose starts********"
 echo "-------$PWD-------"
-docker compose up
+docker compose build
+docker compose up -d
 echo "********Docker container finishes********"
