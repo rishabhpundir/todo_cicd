8eaa68724d41a85003e3f4565ef42f67b50957e3
rishabhpundir <th3rishabh@gmail.com>
2024-04-03 13:40:42 +0530

    updated docker shell file

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 echo "********Docker Compose starts********"
+
 echo "-------$PWD-------"
+
 docker compose build
+
 echo "-------Current images-------"
+
 docker images
+
 echo "-------Current Containers-------"
+
 docker ps -a
+
 echo "********Docker finishes********"
