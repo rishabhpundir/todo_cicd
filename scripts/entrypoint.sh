feb96e6df6b357c7bdb45e149cf7588a420234c6
rishabhpundir <th3rishabh@gmail.com>
2024-04-05 15:02:03 +0530

    updated files

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,6 @@
 echo "*************Applying database migrations*************"
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-
+echo "--------$DB_HOST------$POSTGRES_DB"
 echo "*************Starting the server*************"
 gunicorn --bind 0.0.0.0:8000 todo_cicd.wsgi:application
