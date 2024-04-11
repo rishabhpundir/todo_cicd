719f5a4de873de537f8d511f29aac87ce813900d
rishabhpundir <th3rishabh@gmail.com>
2024-04-11 13:12:50 +0530

    updated k8 shell

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -4,15 +4,13 @@ echo "********Kubenetes Setup starts!********"
 
 cd /var/lib/jenkins/workspace/TodoCICDPipeline
 
-env
-
 /usr/local/bin/minikube status
 
 # /usr/local/bin/minikube start
 
 /usr/local/bin/kubectl delete deployment.apps/todocicd-deploy  deployment.apps/postgres  service/db  service/todocicd-service 
 
-/usr/local/bin/minikube ssh "docker rmi -f todocicd:latest"
+/usr/local/bin/minikube ssh "docker rmi -f todocicd"
 
 /usr/local/bin/minikube ssh "docker images"
 
